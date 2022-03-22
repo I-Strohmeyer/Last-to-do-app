c29cf1019bc8da7efae90b960b1c6b47a4577c18
I-Strohmeyer <50380835+I-Strohmeyer@users.noreply.github.com>
2022-03-23 00:17:43 +0100

    refactor code to use jquery & styles

--- js/script.js
+++ js/script.js
@@ -1,71 +1,42 @@
 function newItem() {
-  //javascript
   //1. Adding a new item to the list of items:
-  let li = document.createElement("li");
-  let inputValue = document.getElementById("input").value;
-  let text = document.createTextNode(inputValue);
-  li.appendChild(text);
+
+  let li = $("<li></li>");
+  let inputValue = $("#input").val();
 
   if (inputValue === "") {
     alert("You must write something!");
   } else {
-    let list = document.querySelector("#list");
-    list.appendChild(li);
+    li.append(inputValue);
+    $("#list").append(li);
+    $("#input").val("");
   }
 
   //2. Crossing out an item from the list of items:
   function crossOut() {
-    li.classList.toggle("strike");
+    li.toggleClass("strike");
   }
 
-  li.addEventListener("dblclick", crossOut);
+  li.on("click", function () {
+    crossOut();
+  });
 
   //3(i). Adding the delete button "X":
-  let crossOutButton = document.createElement("crossOutButton");
-  crossOutButton.appendChild(document.createTextNode("X"));
-  li.appendChild(crossOutButton);
+  let crossOutButton = $("<crossOutButton></crossOutButton>");
+  crossOutButton.append(document.createTextNode("X"));
+  li.append(crossOutButton);
 
-  crossOutButton.addEventListener("click", deleteListItem);
+  //Add event handler for x button
+  crossOutButton.on("click", deleteListItem);
+
   //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
   function deleteListItem() {
-    li.classList.add("delete");
+    li.addClass("delete");
   }
+
   // 4. Reordering the items:
   $("#list").sortable();
 }
-
-/*
-j
-j
-j
-j
-j
-j
-j
-j
-j
-j
-j
-j
-j
-j
-j
-j
-j
-j
-j
-j
-j
-j
-j
-j
-j
-j
-j
-j
-j
-
-*/
 
 // If you get stuck, you can look below for the jQuery code. However, try yourself to convert the vanilla JS code provided to jQuery first.
 
